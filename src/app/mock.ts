a722f5b006bd7026e2e7ddde8aac55fa47f3eaa4
Ana Coronel <anacoronel@hotmail.com>
2021-12-18 12:13:27 -0300

    mock tasks

---
+++ src/app/mock.ts
@@ -0,0 +1,32 @@
+export const TASKS=[
+    {
+        id:1,
+        text: "Terminar Angular",
+        dia: "Lunes 20 diciembre",
+        reminder: true
+
+
+    },
+    {
+        id:2,
+        text: "Terminar leer",
+        dia: "Martes 21 diciembre",
+        reminder: false
+
+
+    },{
+        id:3,
+        text: "Terminar CV",
+        dia: "Miercoles 22 diciembre",
+        reminder: true
+
+
+    },{
+        id:4,
+        text: "Descansar",
+        dia: "Jueves 24 diciembre",
+        reminder: false
+
+
+    },
+]
