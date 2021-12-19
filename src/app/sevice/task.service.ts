0211795d679118b5997240ac142cd214bb36158a
Ana Coronel <anacoronel@hotmail.com>
2021-12-19 11:38:50 -0300

    server json ok

--- src/app/sevice/task.service.ts
+++ src/app/sevice/task.service.ts
@@ -9,14 +9,15 @@ import { TASKS} from '../mock';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = ' http://localhost:5000/tasks'
 
   constructor(
     private http:  HttpClient
   ) { }
 
 getTasks():Observable<Task[]>{
-  const tasks = of(TASKS);
-  return tasks
+  
+  return this.http.get<Task[]>(this.apiUrl)
 }
 
 }
