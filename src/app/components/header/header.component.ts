85e46d5457e60db02e9189507104005bd80ee69f
Ana Coronel <anacoronel@hotmail.com>
2021-12-19 23:34:55 -0300

    agregar tareas, guardar y borrar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../sevice/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title:string = 'Task List';
+  showAddTask: boolean = true;
+  subscription?: Subscription;
 
-  constructor() { }
+
+  constructor(
+    private uiService: UiService,
+
+  ) { 
+    this.subscription = this.uiService.onToggle()
+    .subscribe(value => this.showAddTask = value)
+
+  }
 
   ngOnInit(): void {
   }
 toggleAddTask(){
-  console.log("toggle");
+  this.uiService.toggleAddTask();
+
 }
 }
